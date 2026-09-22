0877e6dbd64fc1a0fe08ef3f743eb28a2d25f265
Vinaymandloi448 <vinay.mandloi.averybit@gmail.com>
2026-09-22 21:00:18 +0530

    feat: initialize Spring Boot project structure and health controller

---
+++ feculty_backend/src/main/java/com/facultyconnect/feculty_backend/FecultyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.facultyconnect.feculty_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FecultyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FecultyBackendApplication.class, args);
+	}
+
+}
---
+++ feculty_backend/src/test/java/com/facultyconnect/feculty_backend/FecultyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facultyconnect.feculty_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FecultyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
